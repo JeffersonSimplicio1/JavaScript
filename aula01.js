088cb8c2b34cb22ad2f7eaf2c859773b67b0417b
Smalls <jefferson.a.simplicio@gmail.com>
2024-09-01 23:23:49 -0300

    iniciando estudos da linguagem de programação javaScript

---
+++ aula01.js
@@ -0,0 +1,19 @@
+// o ; no javascript é opcional.
+// const é uma variavel constante, ou seja, uma vez declarada seu valor não pode ser mudado
+const pi = 3.14;
+// A variavel let é uma variavel que pode ser mudada durante o programa.
+let aluno = 'jefferson';
+// console.log é utilizado para escrever algo na tela mesma função do System.out.println
+console.log('Hello world!!');
+
+let numero1 = 20;
+let soma = numero1 + pi;
+let multiplicacao = numero1 * pi;
+let divisao = numero1 / pi;
+let subtracao = numero1 - soma;
+
+console.log('soma = ', soma)
+console.log('subtracao = ',subtracao)
+console.log('multiplicacao = ', multiplicacao)
+console.log('divisao = ', divisao)
+
---
+++ desafio01.js
@@ -0,0 +1,19 @@
+/*Faça um programa para calcular o valor de uma variavel.
+
+Você terá 3 variaveis. sendo elas: 
+1- preço do combustivel
+2- Gasto médio de combustivel do carro por dia.
+3- Distancia em km da viagem
+
+imprima o valor que sera gasto para realizar esta viagem*/
+
+                                                            
+const precoCombustivel = 5.79;
+const gastoMedio = 10;
+const distacia = 100;
+let valorGasto = precoCombustivel * ( distacia / gastoMedio );
+
+console.log(valorGasto)
+
+
+
