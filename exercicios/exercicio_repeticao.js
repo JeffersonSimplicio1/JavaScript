38a7f90a63144b867abbbf51b8fd13c21c623e16
Smalls <jefferson.a.simplicio@gmail.com>
2024-09-28 22:45:47 -0300

    primeiro exercicio de repetição

---
+++ exercicios/exercicio_repeticao.js
@@ -0,0 +1,9 @@
+// Crie um programa que dado um numero imprima a sua tabuada
+
+const numero = 7;
+
+for (let i = 1; i <= 10; i++) {
+    multiplicao = i * numero;
+    console.log(multiplicao);
+    
+}
